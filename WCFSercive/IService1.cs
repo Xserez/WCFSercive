e82d5c8d4279c2a9dfb54249c87fc70c1729590f
xsere <xsere@XSEREZ>
2023-06-19 00:39:28 +0300

    Proje dosyası ekle.

---
+++ WCFSercive/IService1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFSercive
+{
+    // NOT: "IService1" arabirim adını kodda ve yapılandırma dosyasında birlikte değiştirmek için "Yeniden Düzenle" menüsündeki "Yeniden Adlandır" komutunu kullanabilirsiniz.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string MesajEkle(string value);
+
+        [OperationContract]
+        string MesajGetir();
+    }
+}
